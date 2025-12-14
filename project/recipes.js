1154d0b74bc09dcc8783d9813484cf883704e57f
haydenmueller05 <haydenprmueller@gmail.com>
2025-12-13 21:06:15 -0700

    optimized

--- project/recipes.js
+++ project/recipes.js
@@ -3,18 +3,18 @@ export const recipes = [
     name: "Chocolate Chip Cookies",
     type: "cookies",
     steps: "Mix ingredients, bake at 350°F for 12 minutes.",
-    image: "../images/cookies.png"
+    image: "../images/project/cookies.webp"
   },
   {
     name: "Banana Bread",
     type: "bread",
     steps: "Mash bananas, mix batter, bake at 325°F for 60 minutes.",
-    image: "../images/bananabread.png"
+    image: "../images/project/bananabread.webp"
   },
   {
     name: "Brownies",
     type: "dessert",
     steps: "Mix batter, bake at 350°F for 30 minutes.",
-    image: "../images/brownie.png"
+    image: "../images/project/brownie.webp"
   }
 ];
